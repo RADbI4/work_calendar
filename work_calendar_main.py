e8186201c661da71fef5bfac8bc7f393ac9ce4ab
Radmir <capitanpit@yandex.ru>
2022-12-11 17:42:02 +0300

    next functions

--- work_calendar_main.py
+++ work_calendar_main.py
@@ -1,21 +1,22 @@
 """
 Логика приложения.
 """
-import work_calendar.src.work_calendar_constants as wc_const
-import src.work_calendar_funcs as wc_fucns
+import work_calendar.src.work_calendar_constants as workc_const
+import src.work_calendar_funcs as workc_fucns
 
 
-def wc_main():
+def workc_main():
     """
     Главная логика приложения
     :return:
     """
     # Определим все рабочие дни в году
-    all_work_days_in_current_year = wc_fucns.work_days_in_year(first_day_of_work=wc_const.now_date[0],
-                                                               month_of_work=wc_const.now_date[1],
-                                                               year_of_work=wc_const.now_date[2])
+    all_work_days_in_current_year = workc_fucns.work_days_in_year(first_day_of_work=workc_const.now_date[0],
+                                                                  month_of_work=workc_const.now_date[1],
+                                                                  year_of_work=workc_const.now_date[2])
     print(all_work_days_in_current_year)
+
 
 # Для тестирования и локальных запусков
 if __name__ == "__main__":
-    wc_main()
+    workc_main()
