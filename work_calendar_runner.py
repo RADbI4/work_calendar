1d0c145541f4415f8718872ac9ff57fb1ad3804a
Radmir <capitanpit@yandex.ru>
2022-12-11 19:25:35 +0300

    fixing application

--- work_calendar_runner.py
+++ work_calendar_runner.py
@@ -1,11 +1,11 @@
 """
 Точка входа в приложение
 """
-from work_calendar_main import wc_main
+from work_calendar_main import workc_main
 import traceback
 
 try:
     while True:
-        wc_main()
+        workc_main()
 except Exception:
     print(traceback.format_exc())
