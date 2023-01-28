70858c45f635cf23146c1586da1db939911454b2
Radmir <capitanpit@yandex.ru>
2023-01-28 19:22:48 +0300

    main fixing starting

--- work_calendar_main.py
+++ work_calendar_main.py
@@ -6,18 +6,19 @@ from work_calendar_obj import Work_days_calculator, Table_plotter
 from my_own_funcs import implosive_attractor
 
 
-def workc_main(*date):
+def workc_main(date):
     """
     Главная логика приложения
     :return:
     """
     # Определим данные для следующего класса и таблицу истинности рабочих/выходных дней.
-    one_month_calculations = Work_days_calculator().work_days_in_month(date[0], date[1], date[2])
+    one_month_calculations = Work_days_calculator()
+    one_month_calculations.work_days_in_month(date[0], date[1], date[2])
     Table_plotter().built_table(*one_month_calculations.color_set_mapper, **one_month_calculations.work_days_return)
 
 
 # Для тестирования и локальных запусков
 if __name__ == "__main__":
-    # now_date = [1, 1, 2023]
-    # workc_main()
+    now_date = (1, 1, 2023)
+    workc_main(now_date)
     pass
