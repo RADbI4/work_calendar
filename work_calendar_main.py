68a59c428a16a6b4e7d8ec1f9f7ed4d2d35304c8
Radmir <capitanpit@yandex.ru>
2023-01-28 23:01:47 +0300

    Stable

--- work_calendar_main.py
+++ work_calendar_main.py
@@ -1,7 +1,7 @@
 """
 Логика приложения.
 """
-from work_calendar_obj import Work_days_calculator, Table_plotter
+from work_calendar_obj import Work_days_calculator, Table_plotter, telegram_floader
 import json
 
 
@@ -10,11 +10,16 @@ def workc_main(date):
     Главная логика приложения
     :return:
     """
+
+    content = json.loads(date)
+    date = content.get('date')
     # Определим данные для следующего класса и таблицу истинности рабочих/выходных дней.
-    date = json.loads(date).get('date')
     one_month_calculations = Work_days_calculator()
     one_month_calculations.work_days_in_month(date[0], date[1], date[2])
+    # Создадим картинку на основе данных
     Table_plotter().built_table(*one_month_calculations.color_set_mapper, **one_month_calculations.work_days_return)
+    # Отправим картинку в телеграм
+    telegram_floader(content.get('chat_id'))
 
 
 # Для тестирования и локальных запусков
