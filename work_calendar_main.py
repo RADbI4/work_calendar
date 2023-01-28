3884b7033d86b77818ff190af7ac1f634daf5815
Radmir <capitanpit@yandex.ru>
2023-01-28 21:58:25 +0300

    json load added

--- work_calendar_main.py
+++ work_calendar_main.py
@@ -1,9 +1,8 @@
 """
 Логика приложения.
 """
-import work_calendar.src.work_calendar_constants as workc_const
 from work_calendar_obj import Work_days_calculator, Table_plotter
-from my_own_funcs import implosive_attractor
+import json
 
 
 def workc_main(date):
@@ -12,6 +11,7 @@ def workc_main(date):
     :return:
     """
     # Определим данные для следующего класса и таблицу истинности рабочих/выходных дней.
+    date = json.loads(date).get('date')
     one_month_calculations = Work_days_calculator()
     one_month_calculations.work_days_in_month(date[0], date[1], date[2])
     Table_plotter().built_table(*one_month_calculations.color_set_mapper, **one_month_calculations.work_days_return)
@@ -19,6 +19,7 @@ def workc_main(date):
 
 # Для тестирования и локальных запусков
 if __name__ == "__main__":
-    now_date = (1, 1, 2023)
+    now_date = {"date": [1, 1, 2023]}
+    now_date = json.dumps(now_date)
     workc_main(now_date)
     pass
